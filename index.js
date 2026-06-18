c405184a919985198e7b3370d13aefc0e79d51e7
tanzid-48 <mdtanzid.525@gmail.com>
2026-06-18 09:57:56 +0600

    Conect Mongodb and crete usser Collection

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 app.use(cors());
 app.use(express.json());
@@ -11,6 +12,33 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("LifeVault API is running!");
 });
+
+const uri = process.env.MONGODB_URI;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const db = client.db("life_vault_db");
+   const usersCollection = db.collection("user");
+
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
